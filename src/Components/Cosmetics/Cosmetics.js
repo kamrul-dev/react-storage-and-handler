1adf01e938ff35e311d46a14d0eb3ed422ad78b9
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-20 22:43:40 +0600

    data load from own data.json file inside the public folder

--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -1,39 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const cosmetics = [
-        {
-          "id": "6237588197402c79e7425516",
-          "price": 209,
-          "name": "Kristin Walls"
-        },
-        {
-          "id": "623758816b836bc1e11e67f1",
-          "price": 365,
-          "name": "Ernestine Chambers"
-        },
-        {
-          "id": "62375881aa5758a33d9510da",
-          "price": 291,
-          "name": "Erma Madden"
-        },
-        {
-          "id": "6237588173d0ff7aa73376e3",
-          "price": 318,
-          "name": "Darcy Duran"
-        },
-        {
-          "id": "623758815446dc05a2beb4b8",
-          "price": 252,
-          "name": "Mayer House"
-        },
-        {
-          "id": "62375881c2a1d82953ab1ae3",
-          "price": 337,
-          "name": "Josefina Matthews"
-        }
-      ]
+    const [cosmetics, setCosmetics] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCosmetics(data));
+    }, [])
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
