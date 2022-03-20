d0bde805833ceb7a1b3b881b1bf2faf60d3748bf
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-20 22:36:24 +0600

    Create and Load Fake data

--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { add } from '../../utilities/calculate';
-// import add from '../../utilities/calculate';
+import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const first = 55;
-    const second = 66;
-    const total = add(first, second);
+    const cosmetics = [
+        { id: 1, name: 'Alta', price: 100 },
+        { id: 2, name: 'NailPolish', price: 200 },
+        { id: 3, name: 'Snow', price: 300 },
+        { id: 4, name: 'Soap', price: 400 },
+        { id: 5, name: 'Shampoo', price: 500 },
+    ]
     return (
         <div>
-           <h1>Welcome to my cosmetics store</h1>
-           <p>Total: {total}</p> 
+            <h1>Welcome to my cosmetics store</h1>
+            {
+                cosmetics.map(cosmetic => <Cosmetic
+                    key={cosmetic.id}
+                    cosmetic={cosmetic}
+                ></Cosmetic>)
+            }
         </div>
     );
 };
