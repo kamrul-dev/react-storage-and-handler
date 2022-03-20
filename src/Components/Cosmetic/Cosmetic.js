ba8a6f6bf9821b8789d75439d08812f492c62bce
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-21 00:13:57 +0600

    Remove data to the local storage and delete shopping cart

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, removeFromDb } from '../../utilities/fakedb';
 import './Cosmetic.css'
 
 const Cosmetic = (props) => {
-    const {name, price, id} = props.cosmetic;
+    const { name, price, id } = props.cosmetic;
+
     const addToCart = (id) => {
         addToDb(id);
+    }
+    const removeFromCart = id => {
+        removeFromDb(id);
     }
 
     /* const addToCartWithParam = () => addToCart(id);  */
@@ -15,8 +19,9 @@ const Cosmetic = (props) => {
         <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for: ${price}</p>
-            <p><small>It is has id: {id}</small></p>         
+            <p><small>It is has id: {id}</small></p>
             <button onClick={() => addToCart(id)}>Add to Cart</button>
+            <button onClick={() => removeFromCart(id)}>Remove</button>
         </div>
     );
 };
